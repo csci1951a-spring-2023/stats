df7c671b3c4b3efb9723f91fc79e253937e61af7
jwoo153 <49794075+jwoo153@users.noreply.github.com>
2023-03-16 01:08:36 -0400

    Update run_tests.py

--- code/run_tests.py
+++ code/run_tests.py
@@ -20,18 +20,19 @@ def scenario_one():
     Scenario:
         - A factory calibrates their milk bottle-filling machine to 150ml each and wants to 
         ensure there is no significant deviation in the volume of milk in the bottles they produce. 
-        Out of 100 bottles, 35 had 150ml, 25 had 135ml, and 40 had 160ml. How likely is their 
+        Out of 105 bottles, 5 of them did not have available data, 35 had 150ml, 25 had 135ml, and 40 had 160ml. How likely is their 
         machine calibrated wrongly, given these results?.
     Input:
-        - dataset: A Pandas DataFrame
+        - a Pandas dataframe (written in the stencil for you)
     Output:
         - tstats: Test statistics (float)
         - p-value: P-value (float)
     """
-    #=#=#=#=#=#=#=#=#=# Solution: two sample t-test #=#=#=#=#=#=#=#=#=#
+    #=#=#=#=#=#=#=#=#=# Solution: one sample t-test #=#=#=#=#=#=#=#=#=#
     
     # Here is the parsed data for this scenario!
-    
+    data = [150]*35 + [135]*25 + [160]*40 + [None]*5
+    df = pd.DataFrame(data, columns=['Volume'])
 
     # TODO:
     # Construct all necessary variables needed to make the call to the right function
@@ -42,89 +43,73 @@ def scenario_one():
     # A good habit is to clean that values out before using them later in the code
     # Clean out these None values using drop_incomplete_rows() from the imports
 
-    
+   
 
     # TODO: Return tstats and pvalue
-    return tstats, pvalue
 
 
 def scenario_two():
     """
     Scenario:
-        - The mean height of U.S. adults ages 20 and older is about 66.5
-        inches (69.3 inches for males, 63.8 inches for females). In our
-        sample data, we have a sample of 435 college students from a 
-        single college.  We want to determine whether the mean height of 
-        students at this college is significantly different than the mean 
-        height of U.S. adults ages 20 and older.
+        - We measure the grams of protein in two different brands of energy bars by sampling 
+        from 10 bars in each brand. Brand A had 12, 14, 13, 11, 12, 12, 11, 14, 17, 10 grams in 
+        each of their bars, whereas Brand B had 10, 10, 10, 12, 18, 18, 19, 12, 11, 10 grams each. 
+        How likely are the average grams of protein in the two brands different from one another?
     Input:
-        - dataset: A Pandas DataFrame
+        - dataset: A Pandas DataFrame (written in the stencil for you)
     Output:
         - tstats: Test statistics (float)
         - p-value: P-value (float)
     """
-    #=#=#=#=#=#=#=#=#=# Solution: one sample t-test #=#=#=#=#=#=#=#=#=#
-    # TODO: Construct the necessary variables to make the call to the right
-    # function in stats_tests.py. Don't forget to drop the rows that
-    # contain empty/null values (in the columns that we care about)
-    # before running the statistical test! :)
+    #=#=#=#=#=#=#=#=#=# Solution: two sample t-test #=#=#=#=#=#=#=#=#=#
+    # TODO: Use the given data and construct any necessary variables to make the call to the right
+    # function in stats_tests.py (Note: another variable may not be required!)
     
-
+   
     # TODO: Return tstats and pvalue
-    return tstats, pvalue
 
 
 def scenario_three():
     """
     Scenario:
-        - In the sample data, the students also reported their class quartile
-        (1,2,3,4 for the first, second, third and fourth quartile), as well 
-        as whether they live on campus. We want to test if there is a relationship 
-        between one's class quartile and whether they live on campus.
-        
-        Note: Their class quartile is denoted in the dataset as "Rank".
+        - A study wants to find out if there is a correlation between someone being exposed 
+        to Disease X and getting the disease or vice versa. They record 15 exposed participants 
+        having the disease, 5 unexposed participants having the disease, 20 unexposed not having the disease, 
+        and 7 exposed to the disease not getting it.
     Input:
-        - dataset: A Pandas DataFrame
+        - dataset: A Pandas DataFrame (written in the stencil for you)
     Output:
         - tstats: Test statistics (float)
         - p-value: P-value (float)
     """
     #=#=#=#=#=#=#=#=#=# Solution: chi-squared #=#=#=#=#=#=#=#=#=#
-    # TODO: Construct the necessary variables to make the call to the right
-    # function in stats_tests.py. Don't forget to drop the rows that
-    # contain empty/null values (in the columns that we care about)
-    # before running the statistical test! :)
-    
+    # TODO: Use the given data and construct any  necessary variables to make the call to the right
+    # function in stats_tests.py (Note: another variable may not be required!)
 
+   
     # TODO: Return tstats and pvalue
-    return tstats, pvalue
+    
 
 
 def scenario_four():
     """
     Scenario:
-        - The students in the sample completed all 4 placement tests for
-        four subject areas - English, Reading, Math, and Writing - when they
-        enrolled in the university. The scores are out of 100 points, and are
-        present in the dataset. We want to test if there was a significant 
-        difference between the average of the English test scores and that of 
-        the Math test scores.
+        - A hospital wants to check if their treatment was successful in changing cholesterol levels for their 
+        patients. They tested the 10 patients cholesterol levels in 2010 and then retested the same patients in 2011. 
+        In 2010 the 10 patients cholesterol levels were 110mg, 115mg, 150mg, 120mg, 99mg, 132mg, 182mg, 183mg, 89mg, and 91mg.
+        In 2011 the same 10 patients cholesterol levels were 84mg, 88mg, 88mg, 89mg, 90mg, 90mg, 91mg, 90mg, 90mg, 90mg.
     Input:
-        - dataset: A Pandas DataFrame
+        - dataset: A Pandas DataFrame (written in the stencil for you)
     Output:
         - tstats: Test statistics (float)
         - p-value: P-value (float)
     """
     #=#=#=#=#=#=#=#=#=# Solution: paired t-test #=#=#=#=#=#=#=#=#=#
     # TODO: Construct the necessary variables to make the call to the right
-    # function in stats_tests.py. Don't forget to drop the rows that
-    # contain empty/null values (in the columns that we care about)
-    # before running the statistical test! :)
+    # function in stats_tests.py (Note: another variable may not be required!)
 
     
-    
     # TODO: Return tstats and pvalue
-    return tstats, pvalue
 
 
 ########## DO NOT CHANGE THIS PART OF THE CODE ##########
